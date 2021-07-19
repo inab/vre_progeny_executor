bbf4e0b1552bb9c7adff36dc30e9f7da0d160518
lrodrin <lrodrin@gmail.com>
2021-07-19 16:47:31 +0200

    updated bash and python RUNNER

--- VRE_RUNNER.py
+++ VRE_RUNNER.py
@@ -1,33 +1,32 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-"""
-.. See the NOTICE file distributed with this work for additional information
-   regarding copyright ownership.
+# Copyright 2020-2021 Barcelona Supercomputing Center (BSC), Spain
+# Copyright 2020-2021 Heidelberg University Hospital (UKL-HD), Germany
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-"""
 import argparse
 import sys
 
-from basic_modules.workflow import Workflow
 from utils import logger
 from apps.jsonapp import JSONApp
-from tool.VRE_Tool import RUNNER
+from tool.VRE_Tool import progenyTool
 
 
-class process_WF_RUNNER(Workflow):
+class Wrapper:
     """
-    Functions for demonstrating the pipeline set up.
+    Functions for wrapping the tool set up and execution.
     """
 
     configuration = {}
@@ -38,51 +37,49 @@ class process_WF_RUNNER(Workflow):
         Initialise the tool with its configuration.
 
         :param configuration: a dictionary containing parameters that define how the operation should be carried out,
-        which are specific to each Dorothea.
+        which are specific to PROGENy tool.
         :type configuration: dict
         """
-        logger.debug("Processing Dorothea")
         if configuration is None:
             configuration = {}
 
         self.configuration.update(configuration)
 
-    def run(self, input_files, metadata, output_files, output_metadata):
+    def run(self, input_files, input_metadata, output_files, output_metadata):
         """
-        Main run function for processing a test file.
+        Main run function for running PROGENy tool.
 
-        :param input_files: Dictionary of file locations.
+        :param input_files: Dictionary of input files locations.
         :type input_files: dict
-        :param metadata: Required meta data.
-        :type metadata: dict
-        :param output_files: Locations of the output files to be returned by the pipeline.
+        :param input_metadata: Dictionary of input files metadata.
+        :type input_metadata: dict
+        :param output_files: Dictionary of output files locations expected to be generated.
         :type output_files: dict
-        :param output_metadata:
+        :param output_metadata: List of output files metadata expected to be generated.
         :type output_metadata: list
-        :return: Locations for the output txt (output_files), Matching metadata for each of the files (output_metadata).
+        :return: Generated output files and their metadata.
         :rtype: dict, dict
         """
         try:
-            logger.debug("Initialise the Dorothea")
-            tt_handle = RUNNER(self.configuration)
-            tt_files, tt_meta = tt_handle.run(input_files, metadata, output_files, output_metadata)
+            tt_handle = progenyTool(self.configuration)
+            tt_files, tt_meta = tt_handle.run(input_files, input_metadata, output_files, output_metadata)
             return tt_files, tt_meta
 
         except Exception as error:
-            errstr = "Test Dorothea wasn't processed successfully. ERROR: {}".format(error)
+            errstr = "Tool PROGENy wasn't executed successfully. ERROR: {}".format(error)
             logger.error(errstr)
             raise Exception(errstr)
 
 
-def main_json(config_path, in_metadata_path, out_metadata_path):
+def main_wrapper(config_path, in_metadata_path, out_metadata_path):
     """
     Main function.
 
-    This function launches the app using configuration written in two json files: defineIO.json and input_metadata.json.
+    This function launches the tool using configuration written in two json files: config.json and in_metadata.json.
 
-    :param config_path: path to a valid JSON file containing information on how the tool should be executed.
+    :param config_path: path to a valid VRE JSON file containing information on how the tool should be executed.
     :type config_path: str
-    :param in_metadata_path: path to a valid JSON file containing information on tool inputs.
+    :param in_metadata_path: path to a valid VRE JSON file containing information on tool inputs.
     :type in_metadata_path: str
     :param out_metadata_path: path to write the JSON file containing information on tool outputs.
     :type out_metadata_path: str
@@ -90,29 +87,25 @@ def main_json(config_path, in_metadata_path, out_metadata_path):
     :rtype: bool
     """
     try:
-        logger.info("1. Instantiate and launch the App")
         app = JSONApp()
 
-        result = app.launch(process_WF_RUNNER, config_path, in_metadata_path, out_metadata_path)  # launch the app
-        logger.info("2. App successfully launched; see " + out_metadata_path)
+        result = app.launch(Wrapper, config_path, in_metadata_path, out_metadata_path)
+        logger.progress("Tool successfully executed; see " + out_metadata_path)
         return result
 
     except Exception as error:
-        errstr = "App wasn't successfully launched. ERROR: {}".format(error)
+        errstr = "Tool wasn't successfully executed. ERROR: {}".format(error)
         logger.error(errstr)
         raise Exception(errstr)
 
 
 if __name__ == "__main__":
-
-    # Set up the command line parameters
-    PARSER = argparse.ArgumentParser(description="VRE Dorothea runner")
+    PARSER = argparse.ArgumentParser(description="VRE PROGENy Tool")
     PARSER.add_argument("--config", help="Configuration file", required=True)
     PARSER.add_argument("--in_metadata", help="Location of input metadata file", required=True)
     PARSER.add_argument("--out_metadata", help="Location of output metadata file", required=True)
     PARSER.add_argument("--log_file", help="Location of the log file", required=False)
 
-    # Get the matching parameters from the command line
     ARGS = PARSER.parse_args()
 
     CONFIG = ARGS.config
@@ -122,4 +115,4 @@ if __name__ == "__main__":
     if ARGS.log_file:
         sys.stderr = sys.stdout = open(ARGS.log_file, "w")
 
-    RESULTS = main_json(CONFIG, IN_METADATA, OUT_METADATA)
+    RESULTS = main_wrapper(CONFIG, IN_METADATA, OUT_METADATA)
